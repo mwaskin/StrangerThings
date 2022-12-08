483ee92c63aa7b540c428778969f7c8dcb0df3a6
Matthew Waskin <matthew@waskin.com>
2022-12-08 16:23:49 -0500

    to make git happy

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { fetchMe } from "./api/auth";
-import { fetchPosts } from "./api/posts";
+import { fetchPosts, deletePost } from "./api/posts";
+import { removePostFromState } from "./helpers";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import "./App.css";
+import Profile from "./components/Profile";
 import Register from "./components/Register";
 import LoginForm from "./components/LoginForm";
-import Profile from "./components/Profile";
-=======
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { fetchMe } from './api/auth';
-import { fetchPosts, deletePost } from './api/posts';
-import { removePostFromState } from './helpers';
-import Header from './components/Header';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Register from './components/Register';
-import LoginForm from './components/LoginForm';
->>>>>>> 9123153834501f9db7efce6c3ea187f23e4cb454
 
 import "./App.css";
 
@@ -66,7 +53,11 @@ const App = () => {
 		navToHome();
 	};
 
-<<<<<<< HEAD
+	const removePost = (postId) => {
+		deletePost(token, postId);
+		setPosts(removePostFromState(posts, postId));
+	};
+
 	const navToHome = () => {
 		navigate("/");
 	};
@@ -98,7 +89,12 @@ const App = () => {
 				<Route
 					path="/"
 					element={
-						<Home posts={posts} token={token} updatePosts={updatePosts} />
+						<Home
+							posts={posts}
+							token={token}
+							setPosts={setPosts}
+							removePost={removePost}
+						/>
 					}
 				/>
 				<Route
@@ -115,53 +111,13 @@ const App = () => {
 						/>
 					}
 				/>
-				<Route path="profile" element={<Profile username={user.username} />} />
+				<Route
+					path="profile"
+					element={<Profile user={user} removePost={removePost} />}
+				/>
 			</Routes>
 		</div>
 	);
 };
-=======
-  const signOut = () => {
-    setToken(undefined);
-    localStorage.clear();
-    setUser({});
-    updatePosts()
-    navToHome();
-  }
-
-  const removePost = (postId) => {
-    deletePost(token, postId);
-    setPosts(removePostFromState(posts, postId));
-  }
-
-  const navToHome = () => {
-    navigate('/');
-  }
-
-  const navToRegister = () => {
-    navigate('/register');
-  }
-
-  const navToSignIn = () => {
-    navigate('/signIn');
-  }
-
-  const navToProfile = () => {
-    navigate('/profile');
-  }
-
-  return (
-    <div className='root-container'>
-      <Header user={user} token={token} signOut={signOut} navToRegister={navToRegister} navToSignIn={navToSignIn} navToHome={navToHome} navToProfile={navToProfile}/>
-      <Routes>
-        <Route path='/' element={<Home posts={posts} token={token} setPosts={setPosts} removePost={removePost}/>}/>
-        <Route path='register' element={<Register setToken={setToken} navToHome={navToHome}/>}/>
-        <Route path='signIn' element={<LoginForm setToken={setToken} navToHome={navToHome} updatePosts={updatePosts}/>}/>
-        <Route path='profile' element={<Profile user={user} removePost={removePost}/>} />
-      </Routes>
-    </div>
-  )
-}
->>>>>>> 9123153834501f9db7efce6c3ea187f23e4cb454
 
 export default App;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
-const Message = ({postHead, fromUser, content}) => {
-  return (
-    <>
-    </>
-  )
-}
+const Message = ({ postHead, fromUser, content }) => {
+	return <div></div>;
+};
 
 export default Message;
